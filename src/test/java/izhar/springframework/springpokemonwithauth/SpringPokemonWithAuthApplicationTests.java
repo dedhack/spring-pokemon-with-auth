26e0b61e8487a819f21c39e5fa2a7b72512869bb
izharj <eezhar@gmail.com>
2023-04-17 14:35:06 +0800

    initialize

---
+++ src/test/java/izhar/springframework/springpokemonwithauth/SpringPokemonWithAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package izhar.springframework.springpokemonwithauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPokemonWithAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
