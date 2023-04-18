edc40c7142c998e7c2daddc5d0742c320bc6f5fb
izharj <eezhar@gmail.com>
2023-04-18 22:05:58 +0800

    created an authcontroller with register user endpoint. updated security config to allow requests for /api/auth/** url

--- src/main/java/izhar/springframework/springpokemonwithauth/repository/UserRepository.java
+++ src/main/java/izhar/springframework/springpokemonwithauth/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserRepository, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     // to check user
     Optional<UserEntity> findByUsername(String username);
